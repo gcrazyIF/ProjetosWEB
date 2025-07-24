c591098e3185d42548d2be737c821969492359e8
gcrazyIF <gcs22@aluno.ifal.edu.br>
2025-07-24 08:40:55 -0300

    implementação do java script no pokefinder (com importação de estilização do card)

--- arquivosAulas/api.js
+++ arquivosAulas/api.js
@@ -18,12 +18,15 @@
     })
 */
 
-const search = document.getElementById("search-button");
-async function getPokemon(name){
+const search = document.getElementById('search-button');
+const pokeInput = document.getElementById('pokemon-input');
+const pokemonCard = document.getElementById('pokemon-card-container')
+async function getPokemon(pokemon){
     try {
-        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokemonNameOrId = pokemon.toLowerCase().trim();
+        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`);
         if(!resposta.ok){
-            throw new Error('Erro ao buscar dados!');
+            throw new Error(`Pokémon "${pokemonNameOrId}" não encontrado!`);
         }
         const dados = await resposta.json();
 
@@ -35,15 +38,38 @@ async function getPokemon(name){
         for(let i = 0; i<dados.abilities.length; i++){
             console.log(i+1+"|",dados.abilities[i].ability.name);
         }
+        renderPokemonData(dados);
     } catch (error) {
         console.error('Erro: ',error.message);
     }
 }
 
-search.addEventListener("sumbit", function (event){
-    event.preventDefault();
+function renderPokemonData(pokemonData){
+    
+    const types = pokemonData.types.map(typeInfo => typeInfo.type.name);
+    const typesNames = types.join(', ');
 
-    const pokePesquisa = pokeInput.value.trim();
+    //toFixed define que o valor decimal tenha apenas uma casa decimal
+    const cardPokemon = `
+    <div class="pokemon-card">
+        <h2>${pokemonData.name}</h2>
+        <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}" class="pokemon-image">
+        <ul class="info-list">    
+            <li><strong>ID:</strong> <span>${pokemonData.id}</span> </li>
+            <li><strong>Altura:</strong> <span>${(pokemonData.height/10).toFixed(1)}m</span> </li>
+            <li><strong>Peso:</strong> <span>${(pokemonData.weight/10).toFixed(1)}kg</span> </li>
+            <li><strong>Tipo:</strong> <span>${typesNames}</span> </li>
+        </ul>
+    </div>
+    `
 
-    getPokemon(pokeInput);
-});
+    pokemonCard.innerHTML = cardPokemon;
+}
+
+search.addEventListener('click', function(){
+    const pokemonNameOrId = pokeInput.value.trim();
+
+    if(pokemonNameOrId){
+        getPokemon(pokemonNameOrId);
+    }
+})
